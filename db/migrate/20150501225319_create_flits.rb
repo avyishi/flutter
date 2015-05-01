e624a5ba90c20b2cd5793b9fd64516e229825232
Cheri <avyishi@gmail.com>
2015-05-01 15:58:39 -0700

    Added flit model for collecting mini post and updated db

---
+++ db/migrate/20150501225319_create_flits.rb
@@ -0,0 +1,11 @@
+class CreateFlits < ActiveRecord::Migration
+  def change
+    create_table :flits do |t|
+      t.text :content
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+    add_index :flits, [:user_id, :created_at]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428223018) do
+ActiveRecord::Schema.define(version: 20150501225319) do
+
+  create_table "flits", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "flits", ["user_id", "created_at"], name: "index_flits_on_user_id_and_created_at"
+  add_index "flits", ["user_id"], name: "index_flits_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
