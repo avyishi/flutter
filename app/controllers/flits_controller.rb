d81756a6f2bc5390c4a524e2f615adad04bd8661
Cheri <avyishi@gmail.com>
2015-05-08 14:23:59 -0700

    Updated flits and controllers

--- app/controllers/flits_controller.rb
+++ app/controllers/flits_controller.rb
@@ -1,2 +1,34 @@
 class FlitsController < ApplicationController
+ 
+
+  def create
+  @flit = current_user.flits.build(flit_params)
+    if @flit.save
+      flash[:success] = "Flit created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def new
+    @flit = Flit.new
+  end
+
+  def destroy
+  end
+
+  def index
+    @flit = Flit.all
+  end
+
+  def show
+    @flit = Flit.find(params[:id])
+  end
+
+  private
+
+    def flit_params
+      params.require(:flit).permit(:content)
+    end
 end
