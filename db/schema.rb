9f3f222217c196ca266408f5f0969f36e130b330
Cheri <avyishi@gmail.com>
2015-05-12 17:26:47 -0700

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -47,11 +47,8 @@ ActiveRecord::Schema.define(version: 20150513001434) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
-
 end
